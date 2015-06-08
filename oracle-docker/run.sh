f1cc0b6461af59c7bdb88d1d29e4f415c72c083c
root <root@mesos2.gerritforge.com>
2015-06-08 09:39:08 -0400

    Adds sleep infinity

--- oracle-docker/run.sh
+++ oracle-docker/run.sh
@@ -5,4 +5,4 @@ sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" /u01/app/oracle/product/11.2.0/xe/
 service oracle-xe start;
 echo "ORACLE STARTED";
 /usr/sbin/sshd -D;
-sleep.infinity;
+sleep infinity;
